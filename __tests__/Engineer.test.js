659275a0b3e61b9580409c8a1568022152acac41
Michael Bishop <mrbishop5544@gmail.com>
2022-08-18 19:05:26 -0400

    copy engineer code from manage/employee and make adjustments, test pass

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,2 +1,28 @@
 const Engineer = require('../lib/Engineer');
-jest.mock('../lib/Engineer'); 
+
+test('creating an engineer object inherited from Employee.js', () => {
+    const engineer = new Engineer('Michael', 1, 'Michael@domain.com', 'placeholder');
+
+    expect(engineer.name).toEqual(expect.any(String));
+    expect(engineer.id).toEqual(expect.any(Number));
+    expect(engineer.email).toEqual(expect.any(String));
+
+    console.log(`Engineer Name: ${engineer.name}`);
+    console.log(`Engineer ID: ${engineer.id}`);
+    console.log(`Engineer E-mail: ${engineer.email}`);
+});
+
+test('getting information off of github', () => {
+    const engineer = new Engineer('Michael', 1, 'Michael@domain.com', 'placeholder');
+
+    expect(engineer.github).toEqual(expect.any(String));
+    console.log(engineer.github);
+});
+
+test('creating the role for engineer', () => {
+    const engineer = new Engineer('Michael', 1, 'Michael@domain.com', 'placeholder');
+
+    expect(engineer.getRole()).toBe('Engineer');
+
+    console.log(engineer.getRole());
+});  
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,7 +1,7 @@
 const Manager = require('../lib/Manager');
 
 
-test('creates a manager object invoked from code from Employee.js', () => {
+test('creating a manager object invoked from code from Employee.js', () => {
     const manager = new Manager('Michael', 1, 'Michael@domain.com', '503-1819');
 
     expect(manager.name).toEqual(expect.any(String));
@@ -13,15 +13,15 @@ test('creates a manager object invoked from code from Employee.js', () => {
     console.log(`Manager E-mail: ${manager.email}`);
 });
 
-test('creates an office number', () => {
+test('creating the office number', () => {
     const manager = new Manager('Michael', 2, 'Michael@domain.com', '503-1819');
 
     expect(manager.officeNumber).toEqual(expect.any(String));
     console.log(`Office Number: ${manager.officeNumber}`);
 });
 
-test('creates a role of manager', () => {
-    const manager = new Manager('Michael');
+test('creating the role for manager', () => {
+    const manager = new Manager('Michael', 2, 'Michael@domain.com', '503-1819');
 
     expect(manager.getRole()).toBe('Manager');
 }); 
