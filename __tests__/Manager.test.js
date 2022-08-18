55f05b6990ff4dd1584e7e5e8e28027966064fab
Michael Bishop <mrbishop5544@gmail.com>
2022-08-18 19:00:51 -0400

    fix manager test code, import from employee, test pass

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,18 +1,23 @@
 const Manager = require('../lib/Manager');
 
 
-test('creates a manager object', () => {
-    const manager = new Manager('Michael');
+test('creates a manager object invoked from code from Employee.js', () => {
+    const manager = new Manager('Michael', 1, 'Michael@domain.com', '503-1819');
 
-    expect(manager.name).toBe('Michael');
+    expect(manager.name).toEqual(expect.any(String));
     expect(manager.id).toEqual(expect.any(Number));
     expect(manager.email).toEqual(expect.any(String));
+
+    console.log(`Manager Name: ${manager.name}`);
+    console.log(`Manager ID: ${manager.id}`);
+    console.log(`Manager E-mail: ${manager.email}`);
 });
 
 test('creates an office number', () => {
-    const manager = new Manager('Michael');
+    const manager = new Manager('Michael', 2, 'Michael@domain.com', '503-1819');
 
-    expect(manager.officeNumber).toEqual(expect.any(Number));
+    expect(manager.officeNumber).toEqual(expect.any(String));
+    console.log(`Office Number: ${manager.officeNumber}`);
 });
 
 test('creates a role of manager', () => {
