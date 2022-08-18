233f66d935075ccfc423abd7cd8a7652b7822b59
Michael Bishop <mrbishop5544@gmail.com>
2022-08-18 18:51:35 -0400

    clone repo to other pc, upload employee test pass, manage test fail

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,2 +1,22 @@
 const Manager = require('../lib/Manager');
-jest.mock('../lib/Manager'); 
+
+
+test('creates a manager object', () => {
+    const manager = new Manager('Michael');
+
+    expect(manager.name).toBe('Michael');
+    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.email).toEqual(expect.any(String));
+});
+
+test('creates an office number', () => {
+    const manager = new Manager('Michael');
+
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+});
+
+test('creates a role of manager', () => {
+    const manager = new Manager('Michael');
+
+    expect(manager.getRole()).toBe('Manager');
+}); 
